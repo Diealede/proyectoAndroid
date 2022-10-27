9215e44e6cafd431307d18e723caa9f1cb63e2fe
Diego Denzer <diemer5806@gmail.com>
2022-10-27 10:14:00 -0500

    proyectoAndroid 1.3

--- app/src/main/java/com/example/proyectoandroid/MainActivity.java
+++ app/src/main/java/com/example/proyectoandroid/MainActivity.java
@@ -5,19 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.Theme_ProyectoAndroid);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn1= (Button) findViewById(R.id.btnIniciarSession);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Bienvenido", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this,Inicio.class);
+                startActivity(intent);
+            }
+        });
     }
 
-    public void sendInicio(View view) {
-        Intent intent = new Intent(this,registro.class);
+    public void sendRegistro(View view) {
+        Intent intent = new Intent(this, registro.class);
         startActivity(intent);
-
     }
-
-}
+    }
--- app/src/main/java/com/example/proyectoandroid/registro.java
+++ app/src/main/java/com/example/proyectoandroid/registro.java
@@ -2,7 +2,9 @@ package com.example.proyectoandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class registro extends AppCompatActivity {
 
@@ -11,4 +13,7 @@ public class registro extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
     }
+    public void sendInicio(View view) {
+        Intent intent = new Intent(this,Inicio.class);
+        startActivity(intent);}
 }
